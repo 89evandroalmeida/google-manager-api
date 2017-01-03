a1095becfabfaed035b20404dab688e84b4e1420
Evandro Almeida <evandrodevops@gmail.com>
2017-01-03 14:00:57 +0000

    Token authentication

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,2 +1,13 @@
 class Api::ApiController < ActionController::Base
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      Rails.logger.debug 'ApiController authenticate token'
+      Rails.logger.debug token.inspect
+      @user = InternalUser.where(api_key: token).first
+    end
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < Api::ApiController
   respond_to :json
 
+  before_action :authenticate
+
   def index
     respond_with UserFacade.read_users(params)
   end
