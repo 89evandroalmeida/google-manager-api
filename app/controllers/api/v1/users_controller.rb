1e9ce32fc39b0ff5765a2368e27bf0199419f6bf
Evandro Almeida <evandrodevops@gmail.com>
2017-01-03 17:05:10 +0000

    Creating user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,28 @@
 class Api::V1::UsersController < Api::ApiController
   respond_to :json
-
   before_action :authenticate
 
   def index
     respond_with UserFacade.read_users(params)
+  end
+
+  def create
+    begin
+      expected_params = [
+        :primaryEmail,
+        :password,
+        :orgUnitPath,
+        :givenName,
+        :familyName
+      ]
+      ActionController::Parameters.new(params)
+        .permit(expected_params)
+        .require(expected_params)
+      respond_with UserFacade.create_user(params), :location => nil
+      # ":location => nil" avoids redirection after POST
+    rescue Exception => e
+      respond_with e, :location => nil
+    end
   end
 
 end
--- app/models/user_facade.rb
+++ app/models/user_facade.rb
@@ -1,6 +1,22 @@
 class UserFacade
   include Connection
 
+  def self.create_user params
+    request = {
+      :api_method => @@api.users.insert,
+      :body_object => {
+        :primaryEmail => params[:primaryEmail],
+        :password     => params[:password],
+        :orgUnitPath  => params[:orgUnitPath],
+        :name         => {
+          :givenName    => params[:givenName],
+          :familyName   => params[:familyName]
+        }
+      }
+    }
+    @@client.execute(request).data
+  end
+
   def self.read_users params
     results = []
     request = {
@@ -9,8 +25,8 @@ class UserFacade
     }
     # fetches all the pages
     loop do
-      result = @@client.execute(request)
-      results << JSON.parse(result.body)
+      result = @@client.execute request
+      results << result.data
       break unless result.next_page_token
       request = result.next_page
     end
